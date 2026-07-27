2058560f186b46e46d1f0093dfa52dd30e2976ec
George T. <118488667+gtrupiano@users.noreply.github.com>
2026-07-27 19:17:43 -0400

    Convolution and Deposited Energy Component Implementation (#2)
    * Implemented convolution kernel. It will later need to be integrated into deposited energy and dwell time update components.
    
    * Integrated convolution kernel into desposited energy calculation.

--- CUDA_Implementation/include/convolution.cuh
+++ CUDA_Implementation/include/convolution.cuh
@@ -1,37 +1,55 @@
 /*
- ******************************************************************************
-    File Name  : convolution.cuh
-    Description:
- ******************************************************************************
+******************************************************************************
+   File Name: convolution.cuh
+   Description:
+******************************************************************************
 */
-  
+
 #ifndef CONVOLUTION_CUH
 #define CONVOLUTION_CUH
-  
+
 /*
- ******************************************************************************
- * INCLUDES
- ******************************************************************************
+******************************************************************************
+* INCLUDES
+******************************************************************************
 */
 
 /*
- ******************************************************************************
- * DEFINES, CONSTANTS, ENUMS, STRUCTS
- ******************************************************************************
+******************************************************************************
+* DEFINES, CONSTANTS, ENUMS, STRUCTS
+******************************************************************************
 */
 
+// Kernel constants
+const uint CONVOLUTION_MASK_WIDTH = 65;
+const uint CONVOLUTION_MASK_RADIUS = CONVOLUTION_MASK_WIDTH / 2;
+
+// Portion of input that was convolved
+const uint CONVOLUTION_OUTPUT_TILE_WIDTH = 16;
+// This is needed since at the first and last element, convolution needs the
+// full mask. Since that will extend the input based on the radius of the mask
+// twice since that extention happens at the first and last element.
+const uint CONVOLUTION_INPUT_TILE_WIDTH = (CONVOLUTION_OUTPUT_TILE_WIDTH + CONVOLUTION_MASK_WIDTH - 1);
 
 /*
- ******************************************************************************
- * GLOBAL VARIABLES
- ******************************************************************************
+******************************************************************************
+* GLOBAL VARIABLES
+******************************************************************************
 */
 
 /*
- ******************************************************************************
- * GLOBAL FUNCTION PROTOTYPES
- ******************************************************************************
+******************************************************************************
+* GLOBAL FUNCTION / KERNEL PROTOTYPES
+******************************************************************************
 */
 
+__global__ void convolutionKernel(
+   float *inputImage,
+   int imageWidth,
+   int imageHeight,
+   const float *__restrict__ mask,
+   float *outputImage
+);
 
-#endif CONVOLUTION_CUH
+
+#endif // CONVOLUTION_CUH
